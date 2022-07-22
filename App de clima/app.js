5a531ef797fdc992c81fa1238c1d6b0d20eac6a0
marcoscucatto <marcoscucatto4@gmail.com>
2022-07-21 23:46:26 -0300

    poniendo en practica ejercicios de js

--- App de clima/app.js
+++ App de clima/app.js
@@ -1,9 +1,16 @@
-// Recibe la data del usuario
-const log = position => {
-    console.log(position);
 
+//creamos la constante de la key api
+const API_KEY = "df0f7d1d35a21125181dc6ecc10d03ff";
+
+
+// Recibe la data
+const fetchData = position => {
+    const {latitude , longitude } = position.coords;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}`)
+        .then(response =>  response.json())
+        .then(data => console.log(data))
 }
-//Recibimos la posicion del usuario
+
 const onLoad = () => {
-    navigator.geolocation.getCurrentPosition(log);
+    navigator.geolocation.getCurrentPosition(fetchData);
 }
---
+++ cambiar Color fondo con boton/app.js
@@ -0,0 +1,22 @@
+// METODO 1: ------- FUNCION MUY BASICA SOLO CAMBIA EL FONDO 1 VEZ
+// function btnChanger() {
+//     let bg = document.getElementById("container").style.background = "red";
+//     let x = document.getElementById("btn-color").style.background = "black";
+//     let u = document.getElementById("btn-color").style.color = "white";
+// }
+
+
+// METODO 2 : ---------- Usando Colores definidos en un array.
+// let colors = ["tomato", "grey","red","violet","purple","blue","red","teal"];
+// let color2 = ["white","orange","indigo","pink","lightcoral","black","cyan"];
+
+// function btnChanger(){
+//     let random = Math.floor(Math.random() * colors.length);
+//     let random2 = Math.floor(Math.random() * color2.length);
+//     console.log("Random value: " + random);
+//     console.log("Random2 value: " + random2);
+//     let x = document.getElementById("btn-color").style.background = colors[random];
+//     let y = document.getElementById("btn-color").style.color = color2[random2];
+//     let z = document.getElementById("container").style.background = color2[random2];
+    
+// }
