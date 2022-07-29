6ad0c3458c122b1b01464630fb414766ec95dcf2
marcoscucatto <marcoscucatto4@gmail.com>
2022-07-28 21:32:48 -0300

    reponsive y cuenta regresiva

--- Minipaginaweb/JS/index_dom.js
+++ Minipaginaweb/JS/index_dom.js
@@ -25,13 +25,13 @@ d.addEventListener("DOMContentLoaded", (e) =>{
 
      //youtube es el id, media query min-wi
      responsiveMedia("youtube",
-     "(min-wi:1024px)",
-     "Contenido Movil", 
-     "Contenido Escritorio");
+     "(min-width:1024px)",
+     `<a href="https://www.youtube.com/watch?v=F4neLJQC1_E&ab_channel=NPRMusic" target="_blank" rel="noopener">Ver Video</a>`, 
+     `<iframe width="560" height="315" src="https://www.youtube.com/embed/2nNUnkVnoTI?start=15" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
      responsiveMedia("gmaps",
-     "(min-wi:1024px)",
-     "Contenido Movil", 
-     "Contenido Escritorio");
+     "(min-width:1024px)",
+     `<a href="https://goo.gl/maps/NfTfajoyxxtGw5pFA" target="_blank" rel="noopener">Ver Mapa</a>`, 
+     `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13618.730966150759!2d-64.18332754999999!3d-31.422866000000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a28f63cf2077%3A0xca71e10e4641086b!2sPaseo%20del%20Buen%20Pastor!5e0!3m2!1ses-419!2sar!4v1659009805345!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`);
     });
     
     //esto sirve para hacer una interfaz interactiva.
--- Minipaginaweb/JS/objeto_responsive.js
+++ Minipaginaweb/JS/objeto_responsive.js
@@ -5,4 +5,18 @@ const d = document,
 //mq = media query
 export default function responsiveMedia(id,mq, mobileContent, desktopContent){
     let breakpoint = w.matchMedia(mq);
+    
+    const responsive = (e) => {
+        if(e.matches){ //cuando la media query se cumpla devuelve true, sino false
+            d.getElementById(id).innerHTML = desktopContent;
+        }
+        else{
+        d.getElementById(id).innerHTML = mobileContent;
+        }
+        
+        // console.log("MQ",breakpoint,e.matches);
+    };
+
+    breakpoint.addEventListener("change",responsive);
+    responsive(breakpoint);
 }
